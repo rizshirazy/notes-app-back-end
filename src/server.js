111bea412af4466bbf03770c94115548c6dab3b5
Muhammad Rizki <rizkielshirazy@gmail.com>
2023-01-29 22:12:48 +0700

    S3 storage implemented

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const Inert = require('@hapi/inert');
-const path = require('path');
 
 const _exports = require('./api/exports');
 const authentications = require('./api/authentications');
@@ -17,7 +16,7 @@ const AuthenticationsService = require('./services/postgres/AuthenticationsServi
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const NotesService = require('./services/postgres/NotesService');
 const UsersService = require('./services/postgres/UsersService');
-const StorageService = require('./services/storage/StorageService');
+const StorageService = require('./services/S3/StorageService');
 
 const TokenManager = require('./tokenize/TokenManager');
 const NotesValidator = require('./validator/notes');
@@ -33,9 +32,7 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const storageService = new StorageService(
-    path.resolve(__dirname, 'api/uploads/file/images'),
-  );
+  const storageService = new StorageService();
 
   const server = Hapi.server({
     port: process.env.PORT,
